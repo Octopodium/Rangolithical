94334b7593a69f085192323fcd8380b64cbf70c4
anaclaragl <ag.linhares2@gmail.com>
2025-09-13 01:32:27 -0300

    Boia sem lock quando muito perto

--- Dish-Dwellers-TCC/Assets/Scripts/Barco/GanchoPuxador.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/Barco/GanchoPuxador.cs
@@ -9,30 +9,38 @@ public class GanchoPuxador : MonoBehaviour
     public Vector3 sinkPosition = new Vector3(0, 2, 0);
     public Vector3 targetPosition;
     public GameObject meshBoia;
+    Vector3 startPosition;
+    public Collider meshCollider;
+
+    public void Start(){
+        startPosition = meshBoia.transform.position;
+    }
+
     public void HandlePuxada(){
         barco.IniciarPuxada(transform.position);
     }
 
     public void SinkBoia(){
         ganchavel.enabled = false;
+        meshCollider.enabled = false;
         targetPosition = meshBoia.transform.position - sinkPosition;
         StartCoroutine(MoveBoia(targetPosition));
     }
 
     public void UnsinkBoia(){
         ganchavel.enabled = true;
-        targetPosition = meshBoia.transform.position + sinkPosition;
-        StartCoroutine(MoveBoia(targetPosition));
+        meshCollider.enabled = true;
+        StartCoroutine(MoveBoia(startPosition));
     }
 
     IEnumerator MoveBoia(Vector3 targetPos){
-        Vector3 startPosition = meshBoia.transform.position;
+        Vector3 thisPosition = meshBoia.transform.position;
         float tempo = 0f;
     
         while (tempo <= 1f)
         {
             tempo += Time.deltaTime * 2f;
-            meshBoia.transform.position = Vector3.Lerp(startPosition, targetPos, tempo);
+            meshBoia.transform.position = Vector3.Lerp(thisPosition, targetPos, tempo);
             yield return null;
         }
 
