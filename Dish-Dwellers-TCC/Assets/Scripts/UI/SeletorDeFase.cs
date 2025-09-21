c9d68061d53e631df6f15ee68bf704bbb5e5eb53
anaclaragl <ag.linhares2@gmail.com>
2025-09-21 02:03:11 -0300

    Mudando só algumas coisas em algumas cenas

--- Dish-Dwellers-TCC/Assets/Scripts/UI/SeletorDeFase.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/UI/SeletorDeFase.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 public class SeletorDeFase : MonoBehaviour
 {
     public void IrParaSala(string salaFase){
-        SceneManager.LoadScene(salaFase, LoadSceneMode.Single);
+        if(salaFase != null){
+            SceneManager.LoadScene(salaFase, LoadSceneMode.Single);
+        }
     }
 
 }
