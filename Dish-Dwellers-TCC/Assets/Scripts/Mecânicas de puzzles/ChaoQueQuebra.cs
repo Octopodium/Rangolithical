0664be78169fee855af04187e095f40aa6e9aabe
Tiago Neto <tiago98.ta@gmail.com>
2025-09-21 13:34:40 -0300

    Alteração de rotação no chão que quebra

--- Dish-Dwellers-TCC/Assets/Scripts/Mecânicas de puzzles/ChaoQueQuebra.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/Mecânicas de puzzles/ChaoQueQuebra.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 public class ChaoQueDestroi : MonoBehaviour {
     [Header("Configs do Chão Quebrável")]
     [SerializeField] private float tempoAntesDeCair = 2f;
-    [SerializeField] private float distanciaPlayer = 3f;
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private float distanciaDoChao = 0.1f; 
 
+    [Header("Tamanho da Zona de Checagem")]
     public Vector3 extensaoDoCubo = new Vector3(1.0f, 1.0f, 1.0f);
+
+    [Header("Rotação da Zona")]
+    public Vector3 rotacaoDoCubo = Vector3.zero; 
+
     private bool _playerNoChao;
     private bool estaDestruido = false;
 
     private void FixedUpdate() {
         Vector3 boxCenter = transform.position - (Vector3.up * distanciaDoChao);
-        _playerNoChao = Physics.CheckBox(boxCenter, extensaoDoCubo, Quaternion.identity, playerLayer);
+        Quaternion boxRotation = Quaternion.Euler(rotacaoDoCubo);
+        _playerNoChao = Physics.CheckBox(boxCenter, extensaoDoCubo * 0.5f, boxRotation, playerLayer);
 
-        if (_playerNoChao && !estaDestruido ) {
+        if (_playerNoChao && !estaDestruido) {
             StartCoroutine(DestruirChao());
         }
     }
@@ -31,6 +36,10 @@ public class ChaoQueDestroi : MonoBehaviour {
     private void OnDrawGizmos() {
         Gizmos.color = Color.yellow;
         Vector3 boxCenter = transform.position - (Vector3.up * distanciaDoChao);
-        Gizmos.DrawWireCube(boxCenter, extensaoDoCubo * 2);
+        Quaternion boxRotation = Quaternion.Euler(rotacaoDoCubo);
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(boxCenter, boxRotation, Vector3.one);
+        Gizmos.matrix = rotationMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, extensaoDoCubo);
+        Gizmos.matrix = Matrix4x4.identity;
     }
 }
