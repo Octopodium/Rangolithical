334d0a5d003cabf3a43c544cf3b143fc202dda28
Tiago Neto <tiago98.ta@gmail.com>
2025-09-21 22:20:55 -0300

    Revert "Mudança no Level 1-2"
    This reverts commit cddcac37d58da43bb23fe9e7268bb4cfc48fdd01.

--- Dish-Dwellers-TCC/Assets/Scripts/Desativador.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Desativador : MonoBehaviour {
-    public GameObject objetoDesativavel;
-
-    public void DesativarObjeto() {
-        objetoDesativavel.SetActive(false);
-    }
-}
