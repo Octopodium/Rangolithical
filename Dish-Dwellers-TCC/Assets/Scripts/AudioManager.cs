e48b89199aa4be04dcc298f7769b1e0c9653b3eb
anaclaragl <ag.linhares2@gmail.com>
2025-12-07 23:40:44 -0300

    Tentando corrigir o audio

--- Dish-Dwellers-TCC/Assets/Scripts/AudioManager.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/AudioManager.cs
@@ -14,64 +14,81 @@ public class AudioManager : MonoBehaviour
     public float currentMasterVolume;
 
     private string masterVolumeKey = "Master Volume";
-    public SliderController masterVolumeSlider;
 
     void Awake() {
         if (audioManager == null) {
             audioManager = this;
+            //DontDestroyOnLoad(gameObject);
         }
         else {
             Destroy(gameObject);
             return;
         }
 
-        //DontDestroyOnLoad(gameObject);
-
         audioSource = GetComponent<AudioSource>();
-        audioSource.volume = defaultMasterVolume;
+        
+        LoadVolumeSafe();
     }
 
     void Start(){
-        LoadVolumeSafe();
+        ConfigurarSliders();
+    }
 
-        masterVolumeSlider = FindObjectOfType<SliderController>(true);
-
-        if (masterVolumeSlider != null) {
-            masterVolumeSlider.sliderObject.onValueChanged.AddListener(MudaVolume);
+    void ConfigurarSliders(){
+        SliderController[] sliders = FindObjectsOfType<SliderController>(true);
+        
+        foreach(SliderController slider in sliders){
+            if(slider != null && slider.sliderObject != null){
+                slider.sliderObject.value = currentMasterVolume;
+                
+                slider.sliderObject.onValueChanged.RemoveAllListeners();
+                slider.sliderObject.onValueChanged.AddListener(MudaVolume);
+            }
         }
     }
 
     /// <summary>
     /// Método global para os SFX.
-    /// Basta chamar onde você necessita de um efeito sonoro e escolher seu tipo, que ele ira usar
-    /// diretamente o clipe daquele mesmo tipo contido na lista de sons.
     /// </summary>
-    /// <param name="sons"></param>
-    /// <param name="volume"></param>
     public static void PlaySounds(TiposDeSons sons, float volume = 1f) {
-        try{
-            audioManager.audioSource.PlayOneShot(audioManager.listaDeSons[(int)sons], volume);
-        }catch{
-            Debug.Log($"<color=red>Deu erro no audio");
+        if(audioManager != null && audioManager.audioSource != null){
+            try{
+                audioManager.audioSource.PlayOneShot(
+                    audioManager.listaDeSons[(int)sons], 
+                    volume * audioManager.currentMasterVolume
+                );
+            }
+            catch{
+                Debug.Log($"<color=red>Deu erro no audio</color>");
+            }
         }
     }
 
     public void MudaVolume(float value) {
+        currentMasterVolume = value;
         audioSource.volume = value;
+        
+        if(musicaAtual != null){
+            musicaAtual.volume = value;
+        }
+        
         SaveVolume(masterVolumeKey, value);
     }
 
-    public void SaveVolume(string key, float value){
+    private void SaveVolume(string key, float value){
         PlayerPrefs.SetFloat(key, value);
         PlayerPrefs.Save();
     }
-    private void LoadVolumeSafe()
-    {
+    
+    private void LoadVolumeSafe(){
         currentMasterVolume = PlayerPrefs.GetFloat(masterVolumeKey, defaultMasterVolume);
-        MudaVolume(currentMasterVolume);
-
-        if(masterVolumeSlider != null) {
-            masterVolumeSlider.MudarValueSlider(currentMasterVolume);
+        
+        if(audioSource != null){
+            audioSource.volume = currentMasterVolume;
+        }
+        
+        if(musicaAtual != null){
+            musicaAtual.volume = currentMasterVolume;
         }
     }
 }
--- Dish-Dwellers-TCC/Assets/Scripts/UI/SliderController.cs
+++ Dish-Dwellers-TCC/Assets/Scripts/UI/SliderController.cs
@@ -4,13 +4,27 @@ using UnityEngine.UI;
 public class SliderController : MonoBehaviour
 {
     public Slider sliderObject;
+    private AudioManager audioManager;
 
     public void Awake(){
-        sliderObject.onValueChanged.AddListener(MudarValueSlider);
+        audioManager = FindObjectOfType<AudioManager>();
+        
+        if(sliderObject != null && audioManager != null)
+        {
+            sliderObject.onValueChanged.RemoveAllListeners();
+            sliderObject.onValueChanged.AddListener(audioManager.MudaVolume);
+            
+            if(audioManager != null)
+            {
+                sliderObject.value = audioManager.currentMasterVolume;
+            }
+        }
     }
 
-    public void MudarValueSlider(float value){
-        sliderObject.value = value;
+    public void AtualizarSlider(float value){
+        if(sliderObject != null)
+        {
+            sliderObject.value = value;
+        }
     }
-
 }
